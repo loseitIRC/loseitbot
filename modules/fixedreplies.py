aa88a2737944650fe1292d5ff95056b5e726b2a6
James <SnoopJeDi@gmail.com>
2016-08-31 18:33:18 -0500

    Added source link

--- modules/fixedreplies.py
+++ modules/fixedreplies.py
@@ -1,9 +1,10 @@
 from sopel import module
 
 keywords = {
-'tdee': 'https://loseitirc.github.io/tdeecalc/',
-'ss': 'http://liamrosen.com/fitness.html',
-'docs' : 'https://github.com/loseitIRC/loseitdocs'
+    'tdee': 'https://loseitirc.github.io/tdeecalc/',
+    'ss': 'http://liamrosen.com/fitness.html',
+    'docs' : 'https://github.com/loseitIRC/loseitdocs',
+    'source' : 'https://github.com/loseitIRC/loseitbot'
 }
 
 @module.commands(*keywords.keys())
