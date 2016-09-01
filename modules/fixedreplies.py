a6c7f7b001e9d3993a31157546f4814824581e8e
James <SnoopJeDi@gmail.com>
2016-09-01 14:53:47 -0500

    usda command and some subreddit definitions

--- modules/fixedreplies.py
+++ modules/fixedreplies.py
@@ -1,16 +1,31 @@
 from sopel import module
 
-keywords = {
-    'tdee': 'https://loseitirc.github.io/tdeecalc/',
-    'ss': 'http://liamrosen.com/fitness.html',
-    'docs' : 'https://github.com/loseitIRC/loseitdocs',
-    'source' : 'https://github.com/loseitIRC/loseitbot'
+commands = {
+    'tdee': 'https://loseitirc.github.io/tdeecalc/'
+    ,'ss': 'http://liamrosen.com/fitness.html'
+    ,'docs' : 'https://github.com/loseitIRC/loseitdocs'
+    ,'source' : 'https://github.com/loseitIRC/loseitbot'
+    ,'faq' : 'https://www.reddit.com/r/loseit/wiki/faq'
+    ,'usda' : 'http://www.nal.usda.gov/fnic/foodcomp/search/index.html'
 }
 
-@module.commands(*keywords.keys())
-@module.example('.' + ', .'.join(keywords.keys()))
+patterns = {
+    'NSV\?': 'NSV means non-scale victory.  See https://www.reddit.com/r/loseit/wiki/faq'
+    ,'GW\?': 'GW means goal weight.  See https://www.reddit.com/r/loseit/wiki/faq'
+}
+
+@module.commands(*commands.keys())
+@module.example('.' + ', .'.join(commands.keys()))
 def linkreply(bot, trigger):
-    bot.reply(keywords[trigger.group(1)])
+    bot.reply(commands[trigger.group(1)])
+
+
+def spontaneousReply(bot, trigger):
+    bot.say(patterns[trigger.match.re.pattern])
+
+# This decorates spontaneousReply for every pattern available
+for pattern in patterns.keys():
+    module.rule(pattern)(spontaneousReply)
 
 @module.rule(".*bad bot\W?")
 def whydontyouloveme(bot, trigger):
