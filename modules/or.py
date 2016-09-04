e2dd9745f9b58fa230b48b532ecab565552da0ef
James <SnoopJeDi@gmail.com>
2016-09-03 21:02:35 -0500

    Python protip: don't use 'or' for a function name

--- modules/or.py
+++ modules/or.py
@@ -3,7 +3,7 @@ import random
 
 @module.commands("or")
 @module.example(".or A or B or C")
-def or(bot, trigger):
+def orfunc(bot, trigger):
     """ Choose from a list of things """
     things = trigger.group(2)
     if things is not None and type(things) is str:
