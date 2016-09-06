7d8f639f1ee6e70aa678fd0804eb0cfe7c0c4ca6
James <SnoopJeDi@gmail.com>
2016-09-06 11:49:12 -0500

    More robust nutrient search, fat and carb commands

--- modules/nutrients/__init__.py
+++ modules/nutrients/__init__.py
@@ -3,50 +3,83 @@ from sopel import module
 import os
 import sqlite3
 
-@module.require_owner()
+# Nutr_No constants
+CALORIE = 208
+PROTEIN = 203
+FAT = 204
+CARB = 205
+SUGAR = 269
+FIBER = 291
+
 @module.commands("cal", "calories")
-@module.example("!calories cheese, cheddar")
+@module.example("!calories cheddar cheese")
 def caloriesCmd(bot, trigger):
     """ 
-    Attempts to retrieve the amount of calories per 100g of a food from USDA's NDB SR28
+    Retrieve the amount of calories per 100g of a food from USDA's NDB SR28
     """
-    foodName = trigger.group(2)
     replyFmt = '"%s" has %i kcal per 100 g'
-    if foodName is None:
+    lookup = lookupFoodAndReply(bot, trigger, replyFmt, nutrient=CALORIE)
+
+
+@module.commands("fat")
+@module.example("!fat cheddar cheese")
+def fatCmd(bot, trigger):
+    """ 
+    Retrieve the amount of fat per 100g of a food from USDA's NDB SR28
+    """
+    replyFmt = '"%s" has %.1f g fat per 100 g'
+    lookup = lookupFoodAndReply(bot, trigger, replyFmt, nutrient=FAT)
+
+
+@module.commands("carb[s]?")
+@module.example("!carb cheddar cheese")
+def carbCmd(bot, trigger):
+    """ 
+    Attempts to retrieve the amount of carb per 100g of a food from USDA's NDB SR28
+    """
+    replyFmt = '"%s" has %.1f g carb per 100 g'
+    lookup = lookupFoodAndReply(bot, trigger, replyFmt, nutrient=CARB)
+
+
+def lookupFoodAndReply(bot, trigger, replyFmt, nutrient=CALORIE):
+    foodName = trigger.group(2)
+    if not foodName:
         return False
-    res = lookupFood(foodName + "%")
 
-    if res:
-        bot.reply(replyFmt % (res[1], res[-1]))
-        print(replyFmt % (res[1], res[-1]))
+    # First, let's just try to find the user's whole request
+    res = foodQuery(["%%%s%%" % foodName], nutrient=nutrient)
+
+    if not res:
+        # look for anything that matches all terms
+        foodNames = ["%%%s%%" % fn for fn in foodName.split(' ')]
+        res = foodQuery(foodNames, nutrient=nutrient)
+
+    if res is not False:
+        bot.reply(replyFmt % res)
     else:
-        # if we were given two things to search for, try "secondWord%firstWord%" 
-        foodName = "%".join(foodName.split(' ')[1::-1])
-        res = lookupFood(foodName + "%")
-        if res:
-            bot.reply(replyFmt % (res[1], res[-1]))
-            print(replyFmt % (res[1], res[-1]))
-        else:
-            bot.reply('No results')
+        bot.reply("No results.")
 
-def lookupFood(foodName):
+
+def foodQuery(foodNames, nutrient=CALORIE):
     conn = sqlite3.connect(os.path.dirname(__file__) + '/nutrients.db')
     c = conn.cursor()
-    cmd = '''
-    SELECT food_des.NDB_No, Long_Desc, Shrt_Desc, ComName, Nutr_Val 
+    cmd = ''.join([
+    '''
+    SELECT Long_Desc, Nutr_Val 
     FROM food_des 
     JOIN nut_data 
         ON food_des.NDB_No = nut_data.NDB_No 
-    WHERE Long_Desc LIKE ? 
-        AND Nutr_No = '208' 
-    LIMIT 1;
+    WHERE
     '''
+    ,' AND '.join(['Long_Desc LIKE ?'] * len(foodNames))
+    ,''' AND Nutr_No = '%i' ''' % nutrient
+    ,'LIMIT 1;'
+    ])
 
-#     c.execute(cmd)
-    c.execute(cmd, (foodName,))
+    c.execute(cmd, tuple(foodNames))
 
     foods = c.fetchall()
     if len(foods) is 0:
         return False
     else:
-        return foods[0]
+        return (foods[0][0], foods[0][1])
