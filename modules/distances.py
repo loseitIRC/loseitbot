9c02108db8077629d7626fdee13e3a036f9f84ec
James <SnoopJeDi@gmail.com>
2016-09-27 10:14:13 -0500

    Input validation and memery

--- modules/distances.py
+++ modules/distances.py
@@ -7,7 +7,12 @@ KM_PER_MILE = 1.60934
 def miles_to_km(bot, trigger):
     """ Convert miles to km """
 
-    miles = float(trigger.group(2))
+    try:
+        miles = float(trigger.group(2))
+    except ValueError:
+        bot.reply("Could not understand input.  Please input the distance in km, e.g. !km 26.2")
+        return False
+
     if miles is not None:
         bot.reply("%.1f miles is equal to %.1f km" % (miles, miles * KM_PER_MILE))
 
@@ -17,7 +22,17 @@ def miles_to_km(bot, trigger):
 def km_to_miles(bot, trigger):
     """ Convert km to miles """
 
-    km = float(trigger.group(2))
+    # memes
+    if trigger.group(2).lower().strip() == "sharona":
+        bot.reply("https://www.youtube.com/watch?v=g1T71PGd-J0")
+        return False
+
+    try:
+        km = float(trigger.group(2))
+    except ValueError:
+        bot.reply("Could not understand input.  Please input the distance in mi, e.g. !mi 5")
+        return False
+
     if km is not None:
         bot.reply("%.1f km is equal to %.1f miles" % (km, km / KM_PER_MILE))
 
