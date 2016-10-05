7ef92e0d4647f82314e5d2d91597ef8d36f96b8e
James <SnoopJeDi@gmail.com>
2016-10-04 20:59:59 -0500

    Map command tracks hostmask to prevent nick abuse, thakns sircrazy

--- modules/map.py
+++ modules/map.py
@@ -12,18 +12,24 @@ def loseitmap(bot, trigger):
     if trigger.group(2) is None:
         bot.reply("#loseit user map: %s" % mapurl)
     else:
-        who = trigger.nick
+        host = trigger.host
+        nick = trigger.nick
         with open('/var/www/html/loseit/map/users.json', 'r+') as f:
             where = trigger.group(2)
             userdb = json.load(f)
             locs = geocoder.geocode(where, exactly_one=False)
             print(locs)
+            if locs is None:
+                bot.reply("Can't find that location.")
+                return False
             if len(locs) > 1:
                 bot.reply("Ambiguous location, please be more specific")
+                return False
             else:
-                userdb[who] = [locs[0].latitude, locs[0].longitude]
+                userdb[host] = {"username" : nick, 
+                                "loc" : [locs[0].latitude, locs[0].longitude]}
                 bot.reply("Location set to %s" % locs[0].address)
             f.seek(0)
-            f.write(json.dumps(userdb))
+            f.write(json.dumps(userdb, indent=2))
             f.truncate()
 
