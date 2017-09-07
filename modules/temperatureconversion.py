0b5fb0f109799358436559f20c33e5f9a138d303
Ben <benjamin.mchone@gmail.com>
2017-09-07 13:50:33 -0500

    Add temperature conversion commands (#8)

---
+++ modules/temperatureconversion.py
@@ -0,0 +1,30 @@
+from sopel import module
+
+@module.commands("fa", "fahrenheit")
+@module.example("fa 100")
+def celsius_to_fahrenheit(bot, trigger):
+    """ Convert Celsius to Fahrenheit """
+
+    try:
+        celsius = float(trigger.group(2))
+    except ValueError:
+        bot.reply("Could not understand input.  Please input the temperature in Celsius, e.g. !fa 100")
+        return False
+
+    if celsius is not None:
+        bot.reply("%.1f degrees Celcius is equal to %.1f degrees Fahrenheit" % (celcius, (celcius * 1.8 + 32)))
+
+
+@module.commands("ce", "celsius")
+@module.example("ce 212")
+def fahrenheit_to_celcius(bot, trigger):
+    """ Convert Fahrenheit to Celsius """
+
+    try:
+        fahrenheit = float(trigger.group(2))
+    except ValueError:
+        bot.reply("Could not understand input.  Please input the temperature in Fahrenheit, e.g. !ce 100")
+        return False
+
+    if celsius is not None:
+        bot.reply("%.1f degrees Fahrenheit is equal to %.1f degrees Celsius" % (fahrenheit, (fahrenheit - 32) / 1.8))
