f8e887207af8c642de233a55ab4ee3e62bad4b4c
James Gerity <snoopjedi@gmail.com>
2017-10-03 13:22:42 -0500

    Change #12 to use datetime

--- modules/what.py
+++ modules/what.py
@@ -1,21 +1,21 @@
 from sopel import module
 import re
-import time
+from datetime import datetime
 
 SUBWAY_SANDWICHES = [
-  "Meatball Marinara",
   "Sweet Onion Chicken Teriyaki",
   "Oven Roasted Chicken",
   "Turkey Breast",
   "Italian B.M.T.",
   "Tuna",
   "Black Forest Ham",
+  "Meatball Marinara"
 ]
 
 @module.commands('what', 'wat')
 def what(bot, trigger):
     if trigger.group(2) and re.match(r'is the subway(\s?(.+?)?\s?(sub)? of the day)?', trigger.group(2).lower()):
-        dayofweek = time.strftime("%w")
+        dayofweek = datetime.now().weekday()
         bot.reply("The Subway sub of the day is {}".format(SUBWAY_SANDWICHES[dayofweek]))
     else:
         bot.reply("I only know about subway subs")
