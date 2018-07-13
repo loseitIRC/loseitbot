11ce143538d1a57793e05b5a5288500407270df5
James Gerity <snoopjedi@gmail.com>
2018-07-13 13:28:18 -0500

    Trigger Zensei's easter egg on smaller input set

--- modules/8ball.py
+++ modules/8ball.py
@@ -8,17 +8,24 @@ responses = [
 ]
 
 @module.commands("8")
-@module.commands("?")
 @module.example("!8 is this the real life?")
 def howmany(bot, trigger):
     """ Ask the bot a yes or no question """
-    if trigger.group(2) and trigger.group(2).lower().startswith("are you sure"):
+    if not trigger.group(2):
+        bot.reply(random.choice(responses))
+        return
+    else:
+        text = trigger.group(2).lower()
+    
+    if text.startswith("are you sure"):
         bot.reply("Did I stutter?")
-    elif trigger.group(2) and re.match(r'loves?[\s]+snoopjedi', trigger.group(2).lower()):
+    if text.startswith("boobs?"):
+        bot.reply("https://m.popkey.co/665a53/8q7oO_s-200x150.gif")
+    elif re.match(r'loves?[\s]+snoopjedi', text):
         bot.reply("No one loves SnoopJeDi")
-    elif trigger.group(2) and re.match(r'butts?\??', trigger.group(2).lower()):
+    elif re.match(r'butts?\??', text):
         bot.reply("Butts are great ( ͡° ͜ʖ ͡°) yay butts")
-    elif trigger.group(2) and re.match(r'good?\??', trigger.group(2).lower()):
-        bot.reply("Zensei smells like Fresca")
+    elif trigger.group(2) and re.search(r'good\?+\s*$', trigger.group(2).lower()):
+        bot.reply("Dunno what's good, but Zensei smells like Fresca")
     else:
         bot.reply(random.choice(responses))
