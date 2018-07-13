a60e5e954bf6e7a2852a320f689ff1a06ff696c6
James Gerity <snoopjedi@gmail.com>
2018-07-13 13:26:52 -0500

    Strip trailing ?

--- modules/how.py
+++ modules/how.py
@@ -5,7 +5,7 @@ import random
 @module.example("!how much spam")
 def howmany(bot, trigger):
     """ Ask how many/how much of something """
-    thing = trigger.group(3)
+    thing = trigger.group(3).rstrip('?')
     if thing is not None:
         thing = ' ' + thing.split(" ")[0]
         if thing[-1].isalpha() and thing[-1] is not 's':
